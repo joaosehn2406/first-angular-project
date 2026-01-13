ab25d15e9c0a34c03ed608d78fb5707d7e02115b
joaosehn2406 <jpssantosos5874@gmail.com>
2026-01-13 20:58:00 -0300

    Created header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
