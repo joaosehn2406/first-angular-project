6d08375188ab8395fbe5c4aa2191fd4bd85767d3
joaosehn2406 <jpssantosos5874@gmail.com>
2026-01-15 11:48:30 -0300

    Added onClick function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,4 +15,9 @@ export class UserComponent {
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar
   }
+
+  onClickUser(): void {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
