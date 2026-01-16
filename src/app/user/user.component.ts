241ac36eb3656edc4e90d5161fa198a0caa8e5ed
joaosehn2406 <jpssantosos5874@gmail.com>
2026-01-16 09:49:16 -0300

    Added non-null assertion operator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,9 +7,9 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) userId?: string;
-  @Input({required: true}) userAvatar?: string;
-  @Input({required: true}) userName?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) userAvatar!: string;
+  @Input({required: true}) userName!: string;
   @Output() userSelected = new EventEmitter<string>();
 
   get imagePath() {
