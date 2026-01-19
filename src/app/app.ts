0985529c014f6c2391fb6cc20d0737969fcc2608
joaosehn2406 <jpssantosos5874@gmail.com>
2026-01-19 10:26:06 -0300

    Added @if and @else notations to app view

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,7 +12,7 @@ import {Tasks} from './tasks/tasks';
 })
 export class App {
   users = DUMMY_USERS;
-  userId: string = '';
+  userId?: string;
 
   get getSelectedUser() {
     return this.users.find((user) => user.id === this.userId)
