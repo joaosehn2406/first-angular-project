05b13d83d03c773e26f8ff5339a0458ad593ca15
joaosehn2406 <jpssantosos5874@gmail.com>
2026-01-20 14:11:24 -0300

    Created modal

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,14 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TaskComponent} from './task/task.component';
+import {Modal} from './modal/modal';
 
 type UserTasks = TaskItem[];
 
 @Component({
   selector: 'app-tasks',
   imports: [
-    TaskComponent
+    TaskComponent,
+    Modal
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
@@ -14,9 +16,15 @@ type UserTasks = TaskItem[];
 export class TasksComponent {
   @Input({required: true}) userName?: string;
   @Input({required: true}) userTasks?: UserTasks;
-  @Output() complete = new EventEmitter<string>();
+  @Output() completeTask = new EventEmitter<string>();
+
+  canShowModal: boolean = false;
 
   onCompleteTask(id: string) {
-    this.complete.emit(id);
+    this.completeTask.emit(id);
+  }
+
+  onClickOpenCreationModal() {
+    this.canShowModal = !this.canShowModal
   }
 }
